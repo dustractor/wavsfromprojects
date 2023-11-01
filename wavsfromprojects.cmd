d583ed6275e38172eafd3b5c67b88a8954741444
Shams Kitz <dustractor@gmail.com>
2023-11-01 15:26:56 -0500

    vbs and cmd

---
+++ wavsfromprojects.cmd
@@ -0,0 +1,6 @@
+
+@echo off
+set PATH=%HOMEDRIVE%%HOMEPATH%\ANACON~1;%PATH%
+set PYTHONHOME=%HOMEDRIVE%%HOMEPATH%\anaconda3
+call conda activate
+%PYTHONHOME%\pythonw.exe .\wavsfromprojects.py
---
+++ wavsfromprojects.vbs
@@ -0,0 +1,2 @@
+
+CreateObject("Wscript.Shell").Run "%HOMEDRIVE%%HOMEPATH%\Documents\GitHub\wavsfromprojects\wavsfromprojects.cmd",0,True
